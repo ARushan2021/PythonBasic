3d6505bb1da16673b0e7e271e567467383580a8f
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2023-03-20 12:46:46 +0300

    Delete Task3.py

--- Task3.py
+++
@@ -1,7 +0,0 @@
-numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
-
-def f(x):
-    for y in range(0, x):
-        if x%2 != 0 and x > 50: return 1
-    return 0
-print(list(filter(f, numbers)))
